6c4b974ecb66f1a05de463db3b085472a704fa91
FH Himel <fhhimel17@gmail.com>
2024-01-31 17:39:23 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Student name="rohim"></Student>
+      <Counter></Counter>
     </div>
   );
 }
 
-function Student(props){
-  console.log(props);
+
+function Counter(){
+  const [count , setCount] = useState(0);
+  const increaseCount = () =>{
+    // const newCount = count + 1;
+    setCount(count + 1);
+  }
+  const decreaseCount = () => setCount(count - 1);
   return(
-    <div>
-      <h1>Hello World!!!</h1>
+    <div className='default_style'>
+      <h1>Count : {count} </h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   )
 }
